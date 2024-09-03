c99b3f8893e0fbc67804c36898db01305cf37348
Visibyte <nathanglass26@gmail.com>
2024-09-03 18:44:43 +0100

    login page

---
+++ src/features/auth/components/login-form.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { loginAction } from "../actions/login-action";
+import { type LoginUserSchemaType, loginUserSchema } from "../schema";
+
+function LoginForm() {
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<LoginUserSchemaType>({
+    resolver: zodResolver(loginUserSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: LoginUserSchemaType) => {
+    setError(undefined);
+
+    startTransition(async () => {
+      const { error } = await loginAction(data);
+      if (error) setError(error);
+    });
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {error && <p className="text-red-500">{error}</p>}
+        <Button type="submit" disabled={isPending}>
+          Login
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default LoginForm;
