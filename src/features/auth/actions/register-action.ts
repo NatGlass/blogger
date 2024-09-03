26f29c433472db9fe5b79be4a67924dc1af089ad
Visibyte <nathanglass26@gmail.com>
2024-09-03 16:26:09 +0100

    add output len

--- src/features/auth/actions/register-action.ts
+++ src/features/auth/actions/register-action.ts
@@ -18,6 +18,7 @@ export async function registerAction(
     const passwordHash = await hash(password, {
       memoryCost: 19456,
       timeCost: 2,
+      outputLen: 32,
       parallelism: 1,
     });
 
