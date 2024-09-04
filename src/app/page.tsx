81c3b31cf807afb9a6891d4ed2a1b486030481b4
Visibyte <nathanglass26@gmail.com>
2024-09-04 13:57:53 +0100

    use useSession hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useSession } from "@/contexts/SessionProvider";
+import { logoutAction } from "@/features/auth/actions/logout-action";
+import Link from "next/link";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  const { user, session } = useSession();
+
+  if (!session || !user) {
+    return (
+      <div>
+        Not signed in
+        <div>
+          <Link href="/auth/register">
+            <Button>Register</Button>
+          </Link>
+          <Link href="/auth/login">
+            <Button>Login</Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p> Signed in as {user.email}</p>
+      <form action={logoutAction}>
+        <Button>Logout</Button>
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;
